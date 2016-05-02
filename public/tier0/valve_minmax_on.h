c6affcb2cc7e69f670bd6928993d430ba28cc74c
Daniel <danielga@outlook.pt>
2016-05-02 14:45:23 +0100

    Fixed vmin/vmax not being defined in POSIX environments.

--- public/tier0/valve_minmax_on.h
+++ public/tier0/valve_minmax_on.h
@@ -1,9 +1,7 @@
 //========= Copyright Valve Corporation, All rights reserved. ============//
-#if !defined(POSIX)
 #ifndef vmin
 	#define vmin(a,b)  (((a) < (b)) ? (a) : (b))
 #endif
 #ifndef vmax
 	#define vmax(a,b)  (((a) > (b)) ? (a) : (b))
-#endif
 #endif

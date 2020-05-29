fc59de9b4856d2b7f0c155cae9c4b0ec74286e3e
Daniel <danielga@outlook.pt>
2020-05-29 23:47:21 +0100

    ﻿Updated premake5 scripts to support x86-64

--- tier3/premake5.lua
+++ tier3/premake5.lua
@@ -3,18 +3,20 @@ local current_dir = _SCRIPT_DIR
 function IncludeSDKTier3()
 	IncludePackage("sourcesdk_tier3")
 
-	filter({})
+	local _project = project()
+	print("WARNING: Project '" .. _project.name .. "' included Source SDK 'tier3' library, which is currently not available in x86-64.")
 
-	sysincludedirs(current_dir .. "/../public/tier3")
+	filter("architecture:x86")
+		sysincludedirs(current_dir .. "/../public/tier3")
 
-	filter("system:windows")
-		links("tier3")
+		filter({"architecture:x86", "system:windows"})
+			links("tier3")
 
-	filter("system:macosx")
-		linkoptions(current_dir .. "/../lib/public/osx32/tier3.a")
+		filter({"architecture:x86", "system:macosx"})
+			linkoptions(path.getabsolute(current_dir) .. "/../lib/public/osx32/tier3.a")
 
-	filter("system:linux")
-		linkoptions(current_dir .. "/../lib/public/linux32/tier3.a")
+		filter({"architecture:x86", "system:linux"})
+			linkoptions(path.getabsolute(current_dir) .. "/../lib/public/linux32/tier3.a")
 
 	filter({})
 end

08b689b913c3da65c4cff662e72686f9645baaef
Daniel <danielga@outlook.pt>
2020-06-26 04:29:29 +0100

    Update INetMsgHandler interface

--- public/inetmsghandler.h
+++ public/inetmsghandler.h
@@ -41,6 +41,8 @@ public:
 	virtual void FileDenied(const char *fileName, unsigned int transferID ) = 0;	// a file request was denied by other side
 
 	virtual void FileSent(const char *fileName, unsigned int transferID ) = 0;	// we sent a file
+
+	virtual bool ShouldAcceptFile(const char *fileName, unsigned int transferID) = 0;
 };
 
 #define PROCESS_NET_MESSAGE( name )	\

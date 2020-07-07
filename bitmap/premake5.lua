4cfa60bcd52d2da04e1afcc81f572bc2721049d1
Daniel <danielga@outlook.pt>
2020-07-07 21:27:59 +0100

    Allow multiple runs of project creator files

--- bitmap/premake5.lua
+++ bitmap/premake5.lua
@@ -8,7 +8,7 @@ function IncludeSDKBitmap()
 	links("bitmap")
 
 	if refcount == 1 then
-		include(current_dir .. "/premake5_create_project.lua")
+		dofile(current_dir .. "/premake5_create_project.lua")
 		project(_project.name)
 	end
 end
--- tier1/premake5.lua
+++ tier1/premake5.lua
@@ -18,7 +18,7 @@ function IncludeSDKTier1()
 		links({"vstdlib", "iconv"})
 
 	if refcount == 1 then
-		include(current_dir .. "/premake5_create_project.lua")
+		dofile(current_dir .. "/premake5_create_project.lua")
 	end
 
 	project(_project.name)

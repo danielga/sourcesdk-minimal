cab3e07edc4a41e7e69ea645ea51c1e5c5d1be71
Daniel <danielga@outlook.pt>
2021-04-03 22:45:02 +0100

    Fixed Addon::FileSystem and IAddonSystem::Information definitions

--- public/GarrysMod/Addon.h
+++ public/GarrysMod/Addon.h
@@ -16,16 +16,14 @@ namespace IAddonSystem
 		std::string title;
 		std::string file;
 		std::string tags;
+		std::string placeholder1;
 		uint64_t time_updated;
 		uint64_t wsid;
 		uint64_t creator;
 		uint64_t hcontent_file;
 		uint64_t placeholder4;
 		uint64_t hcontent_preview;
-		uint64_t placeholder6;
-		uint64_t placeholder7;
-		uint16_t placeholder8;
-		bool placeholder9;
+		uint32_t placeholder6;
 	};
 
 	struct UGCInfo
@@ -61,20 +59,24 @@ namespace Addon
 			virtual void ScanForSubscriptions( CSteamAPIContext *, const char * ) = 0;
 			virtual void Think( ) = 0;
 			virtual void SetDownloadNotify( IAddonDownloadNotification * ) = 0;
+			virtual int Notify( ) = 0;
 			virtual bool IsSubscribed( uint64_t ) = 0;
 			virtual const IAddonSystem::Information *FindFileOwner( const std::string & ) = 0;
 			virtual void AddFile( const IAddonSystem::Information & ) = 0;
 			virtual void ClearAllGMAs( ) = 0;
+			virtual void GetSteamUGCFile( uint64_t, bool ) = 0;
+			virtual void UnmountAddon( uint64_t ) = 0;
 			virtual void MountFloatingAddons( ) = 0;
 			virtual void Shutdown( ) = 0;
 			virtual void AddFile( const SteamUGCDetails_t & ) = 0;
 			virtual void AddSubscription( const SteamUGCDetails_t & ) = 0;
 			virtual void AddJob( Job::Base * ) = 0;
-			virtual void Notify( ) = 0;
 			virtual bool HasChanges( ) = 0;
 			virtual void MarkChanged( ) = 0;
 			virtual void AddonDownloaded( IAddonSystem::Information & ) = 0;
+			virtual void NotifyAddonFailedToDownload( IAddonSystem::Information & ) = 0;
 			virtual const std::list<SteamUGCDetails_t> &GetSubList( ) const = 0;
+			virtual void IsAddonValidPreInstall( SteamUGCDetails_t ) = 0;
 			virtual void Load( ) = 0;
 	};
 

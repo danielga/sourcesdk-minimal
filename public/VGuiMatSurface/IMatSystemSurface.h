daf7ad9f21f61a7c840823d82f4d4c2fb7fd5f20
Daniel <danielga@outlook.pt>
2021-12-17 21:56:06 +0000

    Updated VGuiMatSurface public headers

--- public/VGuiMatSurface/IMatSystemSurface.h
+++ public/VGuiMatSurface/IMatSystemSurface.h
@@ -1,4 +1,4 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+//===== Copyright © 1996-2005, Valve Corporation, All rights reserved. ======//
 //
 // Purpose: An extra interface implemented by the material system 
 // implementation of vgui::ISurface
@@ -14,7 +14,7 @@
 #endif
 
 
-#include <vgui/VGUI.h>
+#include <vgui/vgui.h>
 #include "vgui/ISurface.h"
 
 
@@ -24,6 +24,7 @@
 class VMatrix;
 class IMaterial;
 struct InputEvent_t;
+FORWARD_DECLARE_HANDLE( InputContextHandle_t );
 
 
 //-----------------------------------------------------------------------------
@@ -43,14 +44,13 @@ typedef void (*PlaySoundFunc_t)(const char *pFileName);
 // An extra interface implemented by the material system implementation of vgui::ISurface
 //
 //-----------------------------------------------------------------------------
-#define MAT_SYSTEM_SURFACE_INTERFACE_VERSION "MatSystemSurface008"
 class IMatSystemSurface : public vgui::ISurface
 {
 public:
-	// Hook needed to get input to work.
 	// If the app drives the input (like the engine needs to do for VCR mode), 
-	// it can set bLetAppDriveInput to true and call HandleInputEvent for the input events.
-	virtual void AttachToWindow( void *hwnd, bool bLetAppDriveInput=false ) = 0;
+	// it can call this, setting bLetAppDriveInput to true and call 
+	// HandleInputEvent for the input events.
+	virtual void SetAppDrivesInput( bool bLetAppDriveInput ) = 0;
 
 	// Tells the surface to ignore windows messages
 	virtual void EnableWindowsMessages( bool bEnable ) = 0;
@@ -58,15 +58,17 @@ public:
 	// Starts, ends 3D painting
 	// NOTE: These methods should only be called from within the paint()
 	// method of a panel.
-	virtual void Begin3DPaint( int iLeft, int iTop, int iRight, int iBottom, bool bRenderToTexture = true ) = 0;
-	virtual void End3DPaint() = 0;
+	// When manual supersampling of rendertarget is enabled then the render
+	// target will double the width and height of the rendering region if possible, or will otherwise
+	// increase the rendering region to fill up the width/height of the render target maintaining
+	// the aspect ratio.
+	virtual void Begin3DPaint( int iLeft, int iTop, int iRight, int iBottom, bool bSupersampleRT = false ) = 0;
+	virtual void End3DPaint( bool bIgnoreAlphaWhenCompositing = false ) = 0;
 
 	// NOTE: This also should only be called from within the paint()
 	// method of a panel. Use it to disable clipping for the rendering
 	// of this panel.
 	virtual void DisableClipping( bool bDisable ) = 0;
-	virtual void GetClippingRect( int &left, int &top, int &right, int &bottom, bool &bClippingDisabled ) = 0;  // <<<<< NOTE: output flag is *disabled* state, not enabled, to match the rest of the interface
-	virtual void SetClippingRect( int left, int top, int right, int bottom ) = 0;
 
 	// Prevents vgui from changing the cursor
 	virtual bool IsCursorLocked() const = 0;
@@ -79,11 +81,11 @@ public:
 
 	// Some drawing methods that cannot be accomplished under Win32
 	virtual void DrawColoredCircle( int centerx, int centery, float radius, int r, int g, int b, int a ) = 0;
-	virtual int DrawColoredText( vgui::HFont font, int x, int y, int r, int g, int b, int a, PRINTF_FORMAT_STRING const char *fmt, ... ) = 0;
+	virtual void DrawColoredText( vgui::HFont font, int x, int y, int r, int g, int b, int a, PRINTF_FORMAT_STRING const char *fmt, ... ) = 0;
 
 	// Draws text with current font at position and wordwrapped to the rect using color values specified
 	virtual void DrawColoredTextRect( vgui::HFont font, int x, int y, int w, int h, int r, int g, int b, int a, PRINTF_FORMAT_STRING const char *fmt, ... ) = 0;
-	virtual void DrawTextHeight( vgui::HFont font, int w, int& h, PRINTF_FORMAT_STRING const char *fmt, ... ) = 0;
+	virtual void DrawTextHeight( vgui::HFont font, int w, int& h, PRINTF_FORMAT_STRING char *fmt, ... ) = 0;
 
 	// Returns the length of the text string in pixels
 	virtual int	DrawTextLen( vgui::HFont font, PRINTF_FORMAT_STRING const char *fmt, ... ) = 0;
@@ -103,18 +105,20 @@ public:
 	virtual void Set3DPaintTempRenderTarget( const char *pRenderTargetName ) = 0;
 	virtual void Reset3DPaintTempRenderTarget( void ) = 0;
 
-// Gets a material bound to a surface texture ID
+	// Gets a material bound to a surface texture ID
 	virtual IMaterial *DrawGetTextureMaterial( int id ) = 0;
 
-	virtual void GetFullscreenViewportAndRenderTarget( int & x, int & y, int & w, int & h, ITexture **ppRenderTarget ) = 0;
-	virtual void SetFullscreenViewportAndRenderTarget( int x, int y, int w, int h, ITexture *pRenderTarget ) = 0;
+	// Sets the VGui input context
+	virtual void SetInputContext( InputContextHandle_t hContext ) = 0;
 
-	// get texture id for a texture
-	virtual int  DrawGetTextureId( ITexture *pTexture ) = 0;
+	// uploads a part of a texture, used for font rendering
+	virtual void DrawSetSubTextureRGBA( int textureID, int drawX, int drawY, unsigned const char *rgba, int subTextureWide, int subTextureTall ) = 0;
 
-	// begin and end skin composition painting
-	virtual void BeginSkinCompositionPainting() = 0;
-	virtual void EndSkinCompositionPainting() = 0;
+	// Draws a gradient filled rect where the colors may be different 
+	virtual void DrawTexturedSubRectGradient( int x0, int y0, int x1, int y1, float texs0, float text0, float texs1, float text1, Color colStart, Color colEnd, bool bHorizontal ) = 0;
+
+	// Sets texture RGBA with linear scaling (normally it's pointsampling on the DrawSetTextureRGBA call)
+	virtual void DrawSetTextureRGBALinear( int id, const unsigned char *rgba, int wide, int tall ) = 0;
 };
 
 

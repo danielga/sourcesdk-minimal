f39ef1a60715bcd508d4b34d969fc90a26219ced
RaphaelIT7 <raphael.i.t@outlook.de>
2024-04-18 20:10:37 +0200

    Updated BSP limits + Updated Gamemode::System::FindByName
    [+] Gave `Gamemode::System::FindByName` the right return value.
    [#] Updated BSP Limits
    - Raised `MAX_MAP_TEXINFO` to 16384
    - Raised `MAX_MAP_TEXDATA` to 8192
    - Raised `MAX_MAP_OVERLAYS` to 8192

--- public/GarrysMod/Gamemode.h
+++ public/GarrysMod/Gamemode.h
@@ -32,7 +32,7 @@ public:
 	virtual void Refresh( ) = 0;
 	virtual void Clear( ) = 0;
 	virtual const IGamemodeSystem::Information &Active( ) = 0;
-	virtual void FindByName( const std::string & ) = 0;
+	virtual const IGamemodeSystem::Information &FindByName( const std::string & ) = 0;
 	virtual void SetActive( const std::string & ) = 0;
 	virtual const std::list<IGamemodeSystem::Information> &GetList( ) const = 0;
 	virtual bool IsServerBlacklisted( char const* address, char const* hostname, char const* description, char const* gm, char const* map ) = 0;

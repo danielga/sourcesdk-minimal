20d4573ae5ee7edd755e15fc88807fb87fa1a977
RaphaelIT7 <raphael.i.t@outlook.de>
2024-08-02 02:44:06 +0200

    Updated ITraceFilter + CBaseEntity + IClientMessageHandler + CGMODVariant
    This should solve a few problems with CBaseEntity. These things were tested on Windows

--- public/GarrysMod/CGMODVariant.h
+++ public/GarrysMod/CGMODVariant.h
@@ -1,21 +1,41 @@
 #pragma once
 
 /*
-	Types (Type - Number - Conversion):
-	NIL = 0,
-	Number = 1, - *((float*)&value.data)
-	Unknown = 2, - Unknown
-	Bool = 3, - (bool)value.data
-	Vector = 4, - (Vector&)value.data
-	Angle = 5, - (QAngle&)value.data
-	Entity = 6, - Unknown
-	String = 7  - str = (const char*)value.data2 - length = (int)value.data
+	Types:
+	NIL = 0
+	Number = 1
+	Int = 2
+	Bool = 3
+	Vector = 4
+	Angle = 5,
+	Entity = 6
+	String = 7
 */
 
-struct CGMODVariant
+#define CGMODVariant_NIL 0
+#define CGMODVariant_Number 1
+#define CGMODVariant_Int 2
+#define CGMODVariant_Bool 3
+#define CGMODVariant_Vector 4
+#define CGMODVariant_Angle 5
+#define CGMODVariant_Entity 6
+#define CGMODVariant_String 7
+#define CGMODVariant_Count 8
+
+class CBaseHandle;
+struct CGMODVariant // NOTE: It doesn't seem work perfectly with GMOD(I hate m_Ent)
 {
-	unsigned char type; // Used by Push_GMODVariant to determen which function to use to push it.
-	void* data; // Can be a bool, int, float, Vector&, QAngle&
-	void* data2; // Can be a const char* or a float. If the type is a string, data will be the strings length.
-	void* data3; // Will be a float. Only used by Vector and QAngle for the third value.
+	unsigned char type = 0; // Used by Push_GMODVariant to determen which function to use to push it.
+	union {
+		struct {
+			int m_Length;
+			char* m_pString;
+		};
+		bool m_Bool;
+		float m_Float;
+		int m_Int;
+		short m_Ent;
+		Vector m_Vec;
+		QAngle m_Ang;
+	};
 };

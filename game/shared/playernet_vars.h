danielga/sourcesdk-minimal

commit d1dcc358c36d919b8e094d14006e2c6d092fdd45

Add missing includes and fix 2 function calls (#40)
* Add missing include to animationlayer.h

* Add missing include to c_baseanimating.h

* Add missing includes to c_baseplayer.h

* Add missing includes to c_playerlocaldata.h

* Add missing include to iclientshadowmgr.h

* Remove win32 ifdef in particlemgr.h

This does not seem to work at all.

* Add missing include to collisionproperty.h

* Add missing includes to playernet_vars.h

* Remove unused include in dmattributetypes.h

* Remove std:: namespace from max function calls inside of studio.h

* Remove unclosed endif.

* Revert c59491b

* Revert "Revert c59491b"

This reverts commit 5cfa211e970ed70b1baf84270b665a82667b1419.

game/shared/playernet_vars.h
@@ -11,7 +11,10 @@
 #pragma once
 #endif
 
+#include "networkvar.h"
 #include "shared_classnames.h"
+#include "playernet_vars.h"
+#include "c_baseentity.h"
 
 #define NUM_AUDIO_LOCAL_SOUNDS	8
 

public/studio.h
@@ -1773,7 +1773,7 @@ struct thinModelVertices_t
 	{
 		Assert( ( m_numBoneInfluences  >= 1 ) && ( m_numBoneInfluences  <= 3 ) );
 		Assert( ( boneWeights.numbones >= 1 ) && ( boneWeights.numbones <= m_numBoneInfluences ) );
-		int    numStoredWeights = std::max( 0, ( m_numBoneInfluences - 1 ) );
+		int    numStoredWeights = max( 0, ( m_numBoneInfluences - 1 ) );
 		float *pBaseWeight	= m_boneWeights + vertIndex*numStoredWeights;
 		char  *pBaseIndex	= m_boneIndices + vertIndex*m_numBoneInfluences;
 		for ( int i = 0; i < m_numBoneInfluences; i++ )
@@ -1843,7 +1843,7 @@ private:
 		Assert( pBoneWeights );
 		Assert( ( m_numBoneInfluences <= 1 ) || ( m_boneWeights != NULL ) );
 		Assert( ( m_numBoneInfluences <= 0 ) || ( m_boneIndices != NULL ) );
-		int    numStoredWeights = std::max( 0, ( m_numBoneInfluences - 1 ) );
+		int    numStoredWeights = max( 0, ( m_numBoneInfluences - 1 ) );
 		float *pBaseWeight	= m_boneWeights + vertIndex*numStoredWeights;
 		char  *pBaseIndex	= m_boneIndices + vertIndex*m_numBoneInfluences;
 		float  sum			= 0.0f;

game/client/animationlayer.h
@@ -14,6 +14,7 @@
 #include "rangecheckedvar.h"
 #include "lerp_functions.h"
 #include "networkvar.h"
+#include "client_class.h"
 
 class C_AnimationLayer
 {

game/client/c_baseanimating.h
@@ -26,6 +26,7 @@
 #include "ragdoll_shared.h"
 #include "tier0/threadtools.h"
 #include "datacache/idatacache.h"
+#include "baseentity_shared.h"
 
 #define LIPSYNC_POSEPARAM_NAME "mouth"
 #define NUM_HITBOX_FIRES	10

game/client/c_baseplayer.h
@@ -25,6 +25,8 @@
 #include "c_env_fog_controller.h"
 #include "igameevents.h"
 #include "GameEventListener.h"
+#include "vphysics_interface.h"
+#include "baseplayer_shared.h"
 
 #if defined USES_ECON_ITEMS
 #include "econ_item.h"

game/client/c_playerlocaldata.h
@@ -15,6 +15,8 @@
 #include "basetypes.h"
 #include "mathlib/vector.h"
 #include "playernet_vars.h"
+#include "predictable_entity.h"
+#include "interpolatedvar.h"
 
 //-----------------------------------------------------------------------------
 // Purpose: Player specific data ( sent only to local player, too )

game/client/iclientshadowmgr.h
@@ -13,6 +13,7 @@
 #pragma once
 #endif
 
+#include "predictioncopy.h"
 #include "igamesystem.h"
 #include "icliententityinternal.h"
 #include "engine/ishadowmgr.h"

game/client/particlemgr.h
@@ -119,11 +119,7 @@ entities. Each one is useful under different conditions.
 #include "tier0/fasttimer.h"
 #include "utllinkedlist.h"
 #include "utldict.h"
-#ifdef WIN32
-#include <typeinfo.h>
-#else
 #include <typeinfo>
-#endif
 #include "tier1/utlintrusivelist.h"
 #include "tier1/utlstring.h"
 

commit 6c0d901ffb3ceb4e2b82e4062b578ae9d99ece51

Fix compile issues introduced by #40

game/shared/playernet_vars.h
@@ -14,7 +14,9 @@
 #include "networkvar.h"
 #include "shared_classnames.h"
 #include "playernet_vars.h"
+#ifdef CLIENT_DLL
 #include "c_baseentity.h"
+#endif
 
 #define NUM_AUDIO_LOCAL_SOUNDS	8
 

public/studio.h
@@ -1773,7 +1773,7 @@ struct thinModelVertices_t
 	{
 		Assert( ( m_numBoneInfluences  >= 1 ) && ( m_numBoneInfluences  <= 3 ) );
 		Assert( ( boneWeights.numbones >= 1 ) && ( boneWeights.numbones <= m_numBoneInfluences ) );
-		int    numStoredWeights = max( 0, ( m_numBoneInfluences - 1 ) );
+		int    numStoredWeights = MAX( 0, ( m_numBoneInfluences - 1 ) );
 		float *pBaseWeight	= m_boneWeights + vertIndex*numStoredWeights;
 		char  *pBaseIndex	= m_boneIndices + vertIndex*m_numBoneInfluences;
 		for ( int i = 0; i < m_numBoneInfluences; i++ )
@@ -1843,7 +1843,7 @@ private:
 		Assert( pBoneWeights );
 		Assert( ( m_numBoneInfluences <= 1 ) || ( m_boneWeights != NULL ) );
 		Assert( ( m_numBoneInfluences <= 0 ) || ( m_boneIndices != NULL ) );
-		int    numStoredWeights = max( 0, ( m_numBoneInfluences - 1 ) );
+		int    numStoredWeights = MAX( 0, ( m_numBoneInfluences - 1 ) );
 		float *pBaseWeight	= m_boneWeights + vertIndex*numStoredWeights;
 		char  *pBaseIndex	= m_boneIndices + vertIndex*m_numBoneInfluences;
 		float  sum			= 0.0f;
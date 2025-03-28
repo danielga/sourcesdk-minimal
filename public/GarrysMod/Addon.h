e211e79113edd946a713d9c0aa62158b3d28e3b2
RaphaelIT7 <raphael.i.t@outlook.de>
2025-03-28 18:07:51 +0100

    garrysmod: update addonsystem again

--- public/GarrysMod/Addon.h
+++ public/GarrysMod/Addon.h
@@ -35,6 +35,8 @@ namespace IAddonSystem
 		uint64_t creator;
 		uint32_t pubdate;
 	};
+
+	struct AddonSource; // unknown
 }
 
 namespace Addon
@@ -49,14 +51,13 @@ namespace Addon
 		public:
 			virtual void Clear( ) = 0;
 			virtual void Refresh( ) = 0;
-			virtual int MountFile( const std::string &, std::vector<std::string> * ) = 0;
-			virtual bool ShouldMount( const std::string & ) = 0;
+			virtual int MountFile( const std::string& gmaPath, std::vector<std::string>* files, uint64_t, uint64_t, IAddonSystem::AddonSource) = 0;
 			virtual bool ShouldMount( uint64_t ) = 0;
-			virtual void SetShouldMount( const std::string &, bool ) = 0;
-			virtual void Save( ) = 0;
+			virtual void SetShouldMount( uint64_t, bool ) = 0;
+			virtual bool Save( ) = 0;
 			virtual const std::list<IAddonSystem::Information> &GetList( ) const = 0;
 			virtual const std::list<IAddonSystem::UGCInfo> &GetUGCList( ) const = 0;
-			virtual void ScanForSubscriptions( CSteamAPIContext *, const char * ) = 0;
+			virtual void ScanForSubscriptions( CSteamAPIContext *, const char *, bool ) = 0;
 			virtual void Think( ) = 0;
 			virtual void SetDownloadNotify( IAddonDownloadNotification * ) = 0;
 			virtual int Notify( ) = 0;
@@ -64,11 +65,12 @@ namespace Addon
 			virtual const IAddonSystem::Information *FindFileOwner( const std::string & ) = 0;
 			virtual void AddAddon( const IAddonSystem::Information & ) = 0;
 			virtual void ClearUnusedGMAs( ) = 0;
-			virtual const char* GetAddonFilepath( uint64_t, bool ) = 0;
-			virtual void UnmountAddon( uint64_t ) = 0;
+			virtual const std::string& GetAddonFilepath( uint64_t, bool ) = 0;
+			virtual void UnmountAddon( uint64_t, const char* ) = 0;
 			virtual void UnmountServerAddons( ) = 0;
+			virtual void IsAddonValidPreInstall( SteamUGCDetails_t ) = 0;
 			virtual void Shutdown( ) = 0;
-			virtual void AddJob( Job::Base * ) = 0;
+			virtual void AddJob( Addon::Job::Base * ) = 0;
 			virtual const std::list<SteamUGCDetails_t> &GetSubList( ) const = 0;
 			virtual void MountFloatingAddons( ) = 0;
 			virtual void AddAddonFromSteamDetails( const SteamUGCDetails_t & ) = 0;
@@ -78,7 +80,6 @@ namespace Addon
 			virtual void MarkChanged( ) = 0;
 			virtual void OnAddonDownloaded( const IAddonSystem::Information & ) = 0;
 			virtual void OnAddonDownloadFailed( const IAddonSystem::Information & ) = 0;
-			virtual void IsAddonValidPreInstall( SteamUGCDetails_t ) = 0;
 			virtual void Load( ) = 0;
 	};
 

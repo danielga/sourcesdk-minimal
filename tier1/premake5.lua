885fab6d26fe35af2c0cf7e7042375111f6a16cc
Daniel <danielga@outlook.pt>
2025-05-15 14:25:47 +0100

    Fix tier1 linking

--- tier1/premake5.lua
+++ tier1/premake5.lua
@@ -6,7 +6,7 @@ function IncludeSDKTier1()
 	local _project = project()
 
 	externalincludedirs(current_dir .. "/../public/tier1")
-	links({"tier1", "LZMA"})
+	links({"tier1", "LZMA", "common"})
 
 	filter("system:windows")
 		links({"vstdlib", "ws2_32", "rpcrt4"})

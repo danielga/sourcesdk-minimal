935a75b2574eaafaed207ee0d1fc44a35443999e
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-07-17 13:18:58 +0200

    public: update IGet

--- public/GarrysMod/IGet.h
+++ public/GarrysMod/IGet.h
@@ -69,7 +69,6 @@ public:
 	virtual IGMod_Audio* Audio() = 0;
 	virtual const char* VersionTimeStr() = 0;
 	virtual IAnalytics Analytics() = 0;
-	virtual const char* BuildName() = 0;
 	virtual void UpdateRichPresense( const char* status ) = 0;
 	virtual void ResetRichPresense() = 0;
 	virtual void FilterText(const char*, char*, int, ETextFilteringContext, CSteamID) = 0;
